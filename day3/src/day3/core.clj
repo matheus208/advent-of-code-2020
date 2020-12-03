398d3db4e0e41d3624b52008fdd37f036cf54e42
Matheus S N <m.serpellone@gmail.com>
2020-12-03 09:22:50 -0300

    Improves naming and documentation

--- day3/src/day3/core.clj
+++ day3/src/day3/core.clj
@@ -2,7 +2,9 @@
   (:gen-class))
 
 (defn find-trees-in-path
-  [lines {cols-skip :cols rows-skip :rows}]
+  "Given a collection of lines and a traversing strategy, find how many trees there are on the path.
+  The strategy map must contain :rows-skip and :cols-skip keys, which are the amount of rows/cols to skip on each hop to reach the next point in the path."
+  [lines {:keys [cols-skip rows-skip]}]
   (let [mod-nth        (fn [coll n] (nth coll (mod n (count coll))))
         keep-every-nth (fn [n] (fn [index item] (when (= 0 (mod index n)) item)))
         is-tree?       (fn [c] (= \# c))]
@@ -20,11 +22,11 @@
 (defn -main
   [& args]
   (let [lines (clojure.string/split-lines (slurp *in*))]
-    (println (find-trees-in-path lines {:cols 3 :rows 1}))
-    (println (->> [{:cols 1 :rows 1}
-                   {:cols 3 :rows 1}
-                   {:cols 5 :rows 1}
-                   {:cols 7 :rows 1}
-                   {:cols 1 :rows 2}]
+    (println (find-trees-in-path lines {:cols-skip 3 :rows-skip 1}))
+    (println (->> [{:cols-skip 1 :rows-skip 1}
+                   {:cols-skip 3 :rows-skip 1}
+                   {:cols-skip 5 :rows-skip 1}
+                   {:cols-skip 7 :rows-skip 1}
+                   {:cols-skip 1 :rows-skip 2}]
                   (map #(find-trees-in-path lines %))
                   (apply *)))))
